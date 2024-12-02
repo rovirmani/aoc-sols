7629460004daa2714a3ba74ac7ce39c020e64f29
Rohan Virmani <ro.han@berkeley.edu>
2024-12-02 14:38:15 -0800

    day 2 sol part 1

--- 2024/day02/solution.c
+++ 2024/day02/solution.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 #define MAX_LINE_LENGTH 1024
 
 // Function prototypes
+bool valid_diff(int current, int previous, bool increasing);
 long solve_part1(FILE *fp);
 long solve_part2(FILE *fp);
 
@@ -36,18 +38,70 @@ int main() {
     return 0;
 }
 
+bool valid_diff(int current, int previous, bool increasing) {
+    int diff = current - previous;
+    if (increasing) {
+        return diff > 0 && diff <=3;
+    }
+    else {
+        return diff >= -3 && diff <= -1;
+    }
+}
+
 long solve_part1(FILE *fp) {
     char line[MAX_LINE_LENGTH];
-    long result = 0;
+    long num_safe = 0;
     
     while (fgets(line, sizeof(line), fp)) {
         // Remove newline if present
         line[strcspn(line, "\n")] = 0;
         
-        // TODO: Implement part 1 solution
+        // designed to only process positive ints
+        char *ptr = line;
+        char *endptr;
+        int previous = -1;
+        int current;
+        bool increasing;
+        bool safe = false;
+
+        while (*ptr) {
+            current = strtol(ptr, &endptr, 10);
+            if (ptr == endptr) {
+                break;
+            }
+
+            if (previous == -1) {
+                previous = current;
+            } else {
+                // sequence checking done here
+                if (!safe) {
+                    increasing = (current > previous);
+                    safe = valid_diff(current, previous, increasing);
+                    if (!safe) {
+                        break;
+                    }
+                } else {
+                    if (increasing && !valid_diff(current, previous, increasing) ||
+                    (!increasing && !valid_diff(current, previous, increasing))) {
+                        safe = false;
+                        break;
+                    }
+                }
+                previous = current;
+            }
+
+            ptr = endptr;
+            while(*ptr == ' ') {
+                ptr++;
+            }
+            
+        }
+        if (safe) {
+            num_safe += 1;
+        }
     }
     
-    return result;
+    return num_safe;
 }
 
 long solve_part2(FILE *fp) {
